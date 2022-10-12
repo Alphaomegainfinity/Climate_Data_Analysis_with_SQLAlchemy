af23f12988e71d12a5693ef4bd8172a9792ce477
Halley <brightpearl86@hotmail.com>
2022-10-12 22:10:53 +1100

    v1.0

---
+++ SurfsUp/app.py
@@ -0,0 +1,59 @@
+#setup dependencies
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import func
+from flask import Flask
+from flask import jsonify
+
+
+#Database setup:
+engine = create_engine('sqlite:///Resources/hawaii.sqlite')
+
+#reflect an existing database into a new model
+Base = automap_base()
+#reflect the tables
+Base.prepare(engine, reflect = True)
+
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Flask setup
+app = Flask(__name__)
+
+#Flask Routes:
+
+#creating homepage:
+@app.route("/")
+def homepage():
+    """All available api routes"""
+    return (
+        f"Welcome to the Hawaii API Home Page!<br/>"
+        f"All available Routes as below:<br>"
+        f"Precipitation measurement from the last 12 months: /api/v1.0/precipitation<br>"
+        f"A list of all stations from dataset: /api/v1.0/stations<br>"
+        f"Date and Temperature observations query of the most active station for the past 12 months: /api/v1.0/tobs<br>"
+        f"Retrieve the minimum, maximum, and average temperatures from  the start date onward: /api/v1.0/<start><br>"
+        f"Retrieve the minimum, maximum, and average temperatures from the start date to end date: /api/v1.0/<start>/<end><br>"
+    )
+
+#creating /api/v1.0/precipitation
+@app.route ("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    retrieve_data = session.query(measurement.prcp, str(measurement.date)).filter(measurement.date > '2016-08-22')\
+        .filter(measurement.date <= '2017-08-23').order_by(measurement.date).all()
+
+    session.close()
+    #convert list of tuples into normal list
+    retrieve_list = list(np.ravel(retrieve_data))
+    #convert to json
+    return jsonify (retrieve_list)
+
+
+if __name__ == '__main__':
+    app.run (debug=True)
